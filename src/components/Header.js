d782816a70195a84d88bdddb7503b505b6d86e7c
sultonkul070nortoylokov <sultonkul070nortoylokov@gmail.com>
2021-04-29 13:28:28 +0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import { Icon } from "@iconify/react";
+import LocationIcon from "@iconify/icons-mdi/fire-alert";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>
+        <Icon icon={LocationIcon} />
+        Wildfire Tracker (Powered by NASA)
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
